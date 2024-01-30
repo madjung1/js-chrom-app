b03dac268a946ce7d6ca9feb5fbded1f11d81b5e
madjung1 <50440671+madjung1@users.noreply.github.com>
2024-01-30 19:40:23 +0900

    0130

--- src/app.js
+++ src/app.js
@@ -5,14 +5,16 @@ const loginStatus = document.querySelector("#status");
 const logoutBtn = document.querySelector("#status-user");
 const main = document.querySelector("main")
 const contentsPage = document.querySelector(".contents")
+const clock = document.querySelector("#clock");
+let today = new Date();
+
 
 function loginSubmit(event){
     event.preventDefault();
     const userName = loginInput.value;
     console.log(userName);
     classControl();
-    loginWelcome.innerHTML = `${userName} 님 어서오세요 ! `;
-
+    timeCheck();
     
 }
 function classControl(){
@@ -28,3 +30,31 @@ function classControl(){
 // loginForm.addEventListener("submit", loginSubmit);
 
 loginForm.onsubmit = loginSubmit;
+
+
+function clockFnc(){
+    
+    today = new Date;
+    let hour = today.getHours();
+    let min = today.getMinutes();
+    hour = hour.toString();
+    min = min.toString();
+    const hourPrint = hour.padStart(2,0);
+    const minPrint = min.padStart(2,0);
+    let time = `${hourPrint}:${minPrint}`;
+    clock.innerHTML= time
+};
+function timeCheck(){
+    const userName = loginInput.value;
+    if(6<today.getHours()&&12>today.getHours()){
+        loginWelcome.innerHTML = `${userName} ! Good Moring ! `;
+    }else if(12<today.getHours()&&18>today.getHours()){
+        loginWelcome.innerHTML = `${userName} ! Good Aftternoon ! `;
+    }else if(18<today.getHours()&&24>today.getHours()){
+        loginWelcome.innerHTML = `${userName} ! Good Evening ! `;
+    }else{
+        loginWelcome.innerHTML = `${userName} ! Good Night ! `;
+    }
+}
+//setInterval(clockFnc , 1000);
+setTimeout(clockFnc , 1000);
